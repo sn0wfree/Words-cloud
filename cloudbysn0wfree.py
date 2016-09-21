e141becf34839cfbe0c27a507225e889a3b984ba
sn0wfree <snowfreedom0815@gmail.com>
2016-09-21 22:55:39 +0100

    1

--- cloudbysn0wfree.py
+++ cloudbysn0wfree.py
@@ -5,12 +5,12 @@ from PIL import Image
 
 
 class inputsingletext:
-    text=''
-    color = 0
-    highlight = False
-    textorientation='L2R'
-    language='English'
-
+    #text=''
+    #color = 0
+    #highlight = False
+    #textorientation='L2R'
+    #language='English'
+    kind='text'
     def __init__(self,txt,c,o,lang,h=False):
         self.text = txt
         self.color = c
@@ -19,11 +19,24 @@ class inputsingletext:
         self.language=lang
 
 class shapepicture:
-    picture=''
-    greyscale='grey'
-    size=0
-
+    #picture=''
+    #greyscale='grey'
+    #size=0
     def __init__(self,img,g,s):
         self.picture=img
         self.greyscale=g
         self.size=s
+    def shapeprocess(self):
+        if self.greyscale !='grey':
+            if self.picture.mode not in ('1','L','RGB','CMYK'):
+                raise Exception('That is not a recogized image mode')
+            elif self.picture.mode == 'L':
+                self.greyscale ='grey'
+                pass
+            else:
+                self.picture=self.picture.convert('L')
+                self.greyscale ='grey'
+                pass
+        else:
+            pass
+        return self
