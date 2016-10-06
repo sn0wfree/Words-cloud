ef8b8bbcd7cae021c9324c4c4b4ff378ef6996fa
sn0wfree <snowfreedom0815@gmail.com>
2016-10-06 13:58:28 +0100

    update0.1

--- main.py
+++ main.py
@@ -13,36 +13,36 @@ class initialparameter:
         self.ECCENTRICITY = 1.5
         self.LOWER_START = 0.45
         self.UPPER_START = 0.55
-    def initial(fonts='fonts.json', fonts_dir='fonts',DEFAULT_FONT = 'Droid Sans',DEFAULT_PALETTE = 'default'):
-        FONT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), fonts_dir)
-
-
-        FONT_CACHE = simplejson.load(open(os.path.join(FONT_DIR, fonts), 'r'))
-
-        pygame.init()
-        convsurf = Surface((2 * TAG_PADDING, 2 * TAG_PADDING))
-        convsurf.fill((255, 0, 255))
-        convsurf.set_colorkey((255, 0, 255))
-        draw.circle(convsurf, (0, 0, 0), (TAG_PADDING, TAG_PADDING), TAG_PADDING)
-        CONVMASK = mask.from_surface(convsurf)
-
-        LAYOUT_HORIZONTAL = 0
-        LAYOUT_VERTICAL = 1
-        LAYOUT_MOST_HORIZONTAL = 2
-        LAYOUT_MOST_VERTICAL = 3
-        LAYOUT_MIX = 4
-        LAYOUTS = (
-                   LAYOUT_HORIZONTAL,
-                   LAYOUT_VERTICAL,
-                   LAYOUT_MOST_HORIZONTAL,
-                   LAYOUT_MOST_VERTICAL,
-                   LAYOUT_MIX
-                   )
-
-        LAST_COLLISON_HIT = None
 
 
 
+def initial(fonts='fonts.json', fonts_dir='fonts',DEFAULT_FONT = 'Droid Sans',DEFAULT_PALETTE = 'default'):
+    FONT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), fonts_dir)
+
+
+    FONT_CACHE = simplejson.load(open(os.path.join(FONT_DIR, fonts), 'r'))
+
+    pygame.init()
+    convsurf = Surface((2 * TAG_PADDING, 2 * TAG_PADDING))
+    convsurf.fill((255, 0, 255))
+    convsurf.set_colorkey((255, 0, 255))
+    draw.circle(convsurf, (0, 0, 0), (TAG_PADDING, TAG_PADDING), TAG_PADDING)
+    CONVMASK = mask.from_surface(convsurf)
+
+    LAYOUT_HORIZONTAL = 0
+    LAYOUT_VERTICAL = 1
+    LAYOUT_MOST_HORIZONTAL = 2
+    LAYOUT_MOST_VERTICAL = 3
+    LAYOUT_MIX = 4
+    LAYOUTS = (
+               LAYOUT_HORIZONTAL,
+               LAYOUT_VERTICAL,
+               LAYOUT_MOST_HORIZONTAL,
+               LAYOUT_MOST_VERTICAL,
+               LAYOUT_MIX
+               )
+
+    LAST_COLLISON_HIT = None
 
 
 
